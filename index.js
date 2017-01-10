858a9668db6ea17445e632e90635894dc1476ec2
Alvin FREY <alvin.frey@yahoo.fr>
2017-01-10 15:34:24 +0100

    Mise à jour du script index

--- index.js
+++ index.js
@@ -4,50 +4,21 @@ var telemetry = require("./lib/telemetry");
 var parser = require("./lib/parser");
 var writer = require("./lib/writer");
 
-Serial.list(function (err, testingPorts) {
+var serialPort = new Serial("COM5", {
+    baudRate: 57600,
+    parser: Serial.parsers.readline('\n')
+});
 
-    testingPorts.forEach(function (port) {
+serialPort.on('data', function (serialData) {
 
-        if (port.vendorId == "0x0403") {
-
-            var serialPort = new Serial(port.comName, {
-                baudRate: 57600,
-                parser: Serial.parsers.readline('\n')
-            });
-
-            serialPort.on('data', function (serialData) {
-
-                port.write(writer.createAction(
-                    {
-                        feux: true,
-                        module1: true,
-                        module2: true,
-                        module3: true,
-                        module4: true,
-                        module5: true,
-                        module6: true,
-                        module7: true,
-                        module8: true,
-                        order1: 1,
-                        order2: 1,
-                        order3: 1,
-                        order4: 1
-                    }
-                ));
-
-                process.emit('serial-data', parser.parseMessage(serialData.toString()));
-
-            });
-
-            serialPort.on('disconnect', function () {
-
-                console.log("[SERIAL CONNECTION] La connexion série vient d'être déconnecté ! ".red);
-                process.exit(1);
-
-            });
-
-        }
-
-    });
+    process.emit('serial-data', parser.parseMessage(serialData.toString()));
 
 });
+
+serialPort.on('disconnect', function () {
+
+    console.log("[SERIAL CONNECTION] La connexion série vient d'être déconnecté ! ".red);
+    process.exit(1);
+
+});
+
