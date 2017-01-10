06a6c35abb817f60be4e5578d9207e6ee6b69895
vfrz <fritz.valentin@live.fr>
2017-01-10 16:37:11 +0100

    Detection automatique du port

--- index.js
+++ index.js
@@ -4,21 +4,56 @@ var telemetry = require("./lib/telemetry");
 var parser = require("./lib/parser");
 var writer = require("./lib/writer");
 
-var serialPort = new Serial("COM5", {
-    baudRate: 57600,
-    parser: Serial.parsers.readline('\n')
+Serial.list(function (err, ports) {
+    ports.forEach(function (port) {
+        if (port.vendorId == "0403") {
+            var serialPort = new Serial(port.comName, {
+                baudRate: 57600,
+                parser: Serial.parsers.readline('\n')
+            });
+
+
+            serialPort.on('open', function () {
+
+                console.log("[SERIAL CONNECTION] La connexion série vient de s'ouvrir sur le port : ".green + port.comName.green);
+
+                setTimeout(function () {
+
+
+                    serialPort.write(writer.createAction({
+                        feux: true,
+                        module1: true,
+                        module2: true,
+                        module3: true,
+                        module4: true,
+                        module5: true,
+                        module6: true,
+                        module7: true,
+                        module8: true,
+                        order1: 1,
+                        order2: 1,
+                        order3: 1,
+                        order4: 1
+
+
+                    }));
+
+                }, 3000);
+            });
+
+            serialPort.on('data', function (serialData) {
+
+                process.emit('serial-data', parser.parseMessage(serialData.toString()));
+
+            });
+
+            serialPort.on('disconnect', function () {
+
+                console.log("[SERIAL CONNECTION] La connexion série vient d'être déconnecté ! ".red);
+                process.exit(1);
+
+            });
+        }
+
+    });
 });
-
-serialPort.on('data', function (serialData) {
-
-    process.emit('serial-data', parser.parseMessage(serialData.toString()));
-
-});
-
-serialPort.on('disconnect', function () {
-
-    console.log("[SERIAL CONNECTION] La connexion série vient d'être déconnecté ! ".red);
-    process.exit(1);
-
-});
-
