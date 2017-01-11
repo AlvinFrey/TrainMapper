0540130fa72528b4e9566da580189a6465159174
vfrz <fritz.valentin@live.fr>
2017-01-11 15:23:22 +0100

    modification du script de démarrage

--- startup.sh
+++ startup.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/bash
 
+apt-get install -y nodejs-legacy nodejs npm git
+git clone "https://github.com/AlvinFrey/TrainMapper.git"
+cd TrainMapper
 npm install pm2@latest -g && npm install && pm2 startup
