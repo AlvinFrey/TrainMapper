e9295cfcefd34714db03b355595b7b55895fcec4
vfrz <fritz.valentin@live.fr>
2017-01-11 09:22:46 +0100

    Logger supprimé

--- index.js
+++ index.js
@@ -1,68 +1,61 @@
-var logger = require("./lib/logger");
-logger.startLogger();
+var colors = require('colors');
+var Serial = require("serialport");
+var telemetry = require("./lib/telemetry");
+var parser = require("./lib/parser");
+var writer = require("./lib/writer");
 
-setTimeout(function () {
+Serial.list(function (err, ports) {
+    ports.forEach(function (port) {
 
-    var colors = require('colors');
-    var Serial = require("serialport");
-    var telemetry = require("./lib/telemetry");
-    var parser = require("./lib/parser");
-    var writer = require("./lib/writer");
-
-    Serial.list(function (err, ports) {
-        ports.forEach(function (port) {
-
-            if (port.vendorId == "0403" || port.vendorId == 0x0403) {
-                var serialPort = new Serial(port.comName, {
-                    baudRate: 57600,
-                    parser: Serial.parsers.readline('\n')
-                });
+        if (port.vendorId == "0403" || port.vendorId == 0x0403) {
+            var serialPort = new Serial(port.comName, {
+                baudRate: 57600,
+                parser: Serial.parsers.readline('\n')
+            });
 
 
-                serialPort.on('open', function () {
+            serialPort.on('open', function () {
 
-                    logger.log("La connexion série vient de s'ouvrir sur le port : " + port.comName);
+                console.log("La connexion série vient de s'ouvrir sur le port : " + port.comName);
 
-                    setTimeout(function () {
-
-
-                        serialPort.write(writer.createAction({
-                            feux: true,
-                            module1: true,
-                            module2: true,
-                            module3: true,
-                            module4: true,
-                            module5: true,
-                            module6: true,
-                            module7: true,
-                            module8: true,
-                            order1: 1,
-                            order2: 1,
-                            order3: 1,
-                            order4: 1
+                setTimeout(function () {
 
 
-                        }));
+                    serialPort.write(writer.createAction({
+                        feux: true,
+                        module1: true,
+                        module2: true,
+                        module3: true,
+                        module4: true,
+                        module5: true,
+                        module6: true,
+                        module7: true,
+                        module8: true,
+                        order1: 1,
+                        order2: 1,
+                        order3: 1,
+                        order4: 1
 
-                    }, 3000);
-                });
 
-                serialPort.on('data', function (serialData) {
+                    }));
 
-                    process.emit('serial-data', parser.parseMessage(serialData.toString()));
+                }, 3000);
+            });
 
-                });
+            serialPort.on('data', function (serialData) {
 
-                serialPort.on('disconnect', function () {
+                process.emit('serial-data', parser.parseMessage(serialData.toString()));
 
-                    logger.log("La connexion série vient d'être déconnecté ! ");
-                    process.exit(1);
+            });
 
-                });
-            }
+            serialPort.on('disconnect', function () {
 
-        });
+                console.log("La connexion série vient d'être déconnecté ! ");
+                process.exit(1);
+
+            });
+        }
+
     });
+});
 
-
-}, 1000);
