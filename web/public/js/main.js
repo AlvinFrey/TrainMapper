98384cc000e91fa2018a1ff9867d5846c8e81fd1
Alvin FREY <alvin.frey@yahoo.fr>
2017-01-11 13:09:23 +0100

    Gestion de l'affichage de l'état des capteurs optiques

--- web/public/js/main.js
+++ web/public/js/main.js
@@ -9,11 +9,13 @@ document.getElementById("circuitSVG").addEventListener("load", function() {
 
         data = JSON.parse(data);
 
-        if(data.equipments==[4]){
+        if(data.equipments.indexOf(4)){
 
             if((data.orders.indexOf("11") + 1)!=0){
 
                 var svgID = data.modules[0].toString() + (data.orders.indexOf("11") + 1).toString();
+
+                console.log(svgID);
 
                 svgDoc.getElementById(svgID).style.setProperty("fill", "#27ae60");
                 svgDoc.getElementById(svgID).style.setProperty("stroke", "#27ae60");
